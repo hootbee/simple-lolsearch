f1247b24ff366a908d297e4d49391e6ff7471c0f
hootbee <xx3xx27@gmail.com>
2025-07-04 18:02:01 +0900

    feat: 첫 커

---
+++ src/main/java/com/example/simple_lolsearch/SimpleLolsearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_lolsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLolsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleLolsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simple_lolsearch/SimpleLolsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_lolsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLolsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
